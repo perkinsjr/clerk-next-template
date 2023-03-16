baa47071b2daa3962c80c0b37a10f6173416ec2c
James Perkins <jamesperkins@hey.com>
2023-03-16 08:33:11 -0400

    create next template.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
